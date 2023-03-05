8a2109c87a6cb5a8d8d370cbdca11870bc48e670
AdityaDanturthi <aditya2096@gmail.com>
2023-03-05 03:38:09 -0330

    update

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randint
 
 app = FastAPI()
+
+def randnum():
+    return randint(1,10000)
+
+myPosts = [{"title": "title of post 1", "content": "content of post 1", "id":1}, {"title": "title of post 2", "content": "content of post 2", "id":2}]
 
 class Post(BaseModel):
     title: str
@@ -11,17 +17,33 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
-myPosts = [{"title": "title of post 1", "content": "content of post 1", "id":1}, {"title": "title of post 2", "content": "content of post 2", "id":2}]
-
-@app.get("/")
+@app.get("/posts")
 def root():
-    return {"Message": "Hello, world!"}
+    return {"posts": myPosts}
 
+@app.get("/posts/{id}")
+def getposts_id(id: int):
+    for p in myPosts:
+        if p["id"] == id:
+            return p
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"Post with id: {id} not found!")
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    print(post.dict())
-    return {"Post details": post}
+    temp = randnum()
+    post_dict = post.dict()
+    post_dict["id"] = temp
+    myPosts.append(post_dict)
+    return {"Post details": post_dict}
+
+@app.put("/posts")
+def update_posts(post: Post):
+    temp = randnum()
+    post_dict = post.dict()
+    post_dict["id"] = temp
+    myPosts.append(post_dict)
+    return {"Post details": post_dict}
 
 
 
